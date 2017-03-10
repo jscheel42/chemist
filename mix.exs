d790132b5eaf9130f94a6581d24d8e4f7790a7e3
Joshua Scheel <jscheel42@gmail.com>
2017-03-09 19:09:00 -0800

    remove distillery dep

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Chemist.Mixfile do
   def project do
     [
       app: :chemist,
-      version: "0.3.1",
+      version: "0.4.0",
       name: "Chemist",
       description: "Chemist is an Elixir wrapper for the Riot API.",
       source_url: "https://github.com/jscheel42/chemist",
@@ -25,7 +25,6 @@ defmodule Chemist.Mixfile do
 
   defp deps do
     [
-      { :distillery,  "~> 1.1"                },
       { :ex_doc,      "~> 0.14.5", only: :dev },
       { :httpoison,   "~> 0.11"               },
       { :poison,      "~> 2.2"                }
